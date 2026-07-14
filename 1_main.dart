7d78570607e48097694feeb72341a99c9ec74f14
Risnanto73 <tiolast63@gmail.com>
2026-07-14 10:34:34 +0700

    first commit

---
+++ 1_main.dart
@@ -0,0 +1,6 @@
+main() {
+  print("hello world!");
+  print("hello world!");
+  // Copy 1 line kebawah => shift + alt + arah bawah
+  // == === => <= >= ==> <==hdkua
+}
---
+++ 2_comment.dart
@@ -0,0 +1,45 @@
+// Single Comment
+// Digunakan menonaktifkan kode baris atau
+// menandakan sebuah catatan atau note
+
+/**Multiline comment
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ * biasaya digunakan untuk menjelaskan sebuah function / logika
+ */
+
+/// documentaion comment [main]
+/// aturan dari documentation comment 
+/// 1. ditandi dengan ///
+/// 2. untuk memanggil nama fucntion harus menggunakan []
+/// [age]
+
+void main(List<String> args) {
+  // mencentak Hello Ucup!
+  print('Hello Ucup!');
+
+  print('object');
+}
+
+int age() {
+  return 17;
+}
