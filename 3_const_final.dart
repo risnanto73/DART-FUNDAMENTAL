045a24121699e60f7ad7bad6f985402d7b58da5c
Risnanto73 <tiolast63@gmail.com>
2026-07-14 11:14:23 +0700

    Implement main function to calculate and print circle area and user name

--- 3_const_final.dart
+++ 3_const_final.dart
@@ -4,6 +4,17 @@
 
 const num pi = 3.14;
 
-void main(List<String> args) {
-  
+void main() {
+  var radius = 10; // bisa diubah (mutable) karena ada var
+
+  print('Luas Lingakaran dengan radius $radius = ${luasLingkaran(radius)}');
+
+  final firstName = 'Ucup';
+  final lastName = 'Guerero';
+
+  // lastName = 'Wiraro';
+
+  print('Namaku $firstName $lastName');
 }
+
+num luasLingkaran(num radius) => pi * radius * radius;
